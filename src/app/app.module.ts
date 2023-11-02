6da93cd7a119a45cd6845174e051655ac254021b
Ankur1326 <swamiankur46@gmail.com>
2023-11-02 14:48:45 +0530

    Signed-off-by: Ankur1326 <swamiankur46@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SellerAuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent {
+
+  singUp(data:object):void {
+    console.log(data);
+  }
+
+}
