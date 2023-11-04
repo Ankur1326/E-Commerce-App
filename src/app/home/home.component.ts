11c9ed476bb57f0e44ce14141730705628e8998d
Ankur1326 <swamiankur46@gmail.com>
2023-11-04 23:04:48 +0530

    install bootstarp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+// import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  // imports: [NgbAlert],
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
 }
